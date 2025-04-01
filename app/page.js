faa76fbcda5411b6d2dba73594dc5c55c52ca262
azim0nt <kktotov457@gmail.com>
2025-04-01 20:25:41 +0500

    create marquee

--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
 'use client'
 import HomeScreen from "./components/HomeScreen";
+import Marquee from "./components/Marquee";
 export default function Home() {
   return (
     <>
     <HomeScreen/>
+    <Marquee/>     
+    
     </>
   );
 }
